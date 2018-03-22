d3e75944276294a7191e471afd7306441b3776df
rgreen88 <rynelgreen@gmail.com>
2018-03-22 18:09:13 -0400

    added register/unregisterListeners

--- sensorhelper/src/main/java/com/example/android/sensorhelper/SensorHelper.java
+++ sensorhelper/src/main/java/com/example/android/sensorhelper/SensorHelper.java
@@ -19,18 +19,41 @@ public class SensorHelper extends Activity implements SensorEventListener {
 
     public SensorHelper() {
         mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
-        assert mSensorManager != null;
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
+        //if statement designating action as long as movement occurs
+        if (mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
+            //do action here
+            Sensor s = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+        }
+        //instantiating all double values to 0
+        x = y = sensorX = sensorY = 0;
     }
 
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
     }
 
-    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //test accuracy of changed event
+    }
 
+    public void onSensorChanged(SensorEvent event) {
+        //Thread sleep to prevent processing each time a slight move occurs
+        try {
+            Thread.sleep(16);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //called when accelerometer senses directional change
+        sensorX = event.values[0];
+        sensorY = event.values[1];
     }
 }
