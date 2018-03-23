8abf0b283460199ad9a9ac518594a2de9911af63
rgreen88 <rynelgreen@gmail.com>
2018-03-23 16:38:08 -0400

    added interface, removed Activity, added constructor to provide context

--- app/src/main/java/com/example/android/sensorlibtest/MainActivity.java
+++ app/src/main/java/com/example/android/sensorlibtest/MainActivity.java
@@ -2,17 +2,8 @@ package com.example.android.sensorlibtest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
-
-import com.example.android.sensorhelper.SensorHelper;
 
 public class MainActivity extends AppCompatActivity {
-
-    SensorHelper mSensorHelper;
-    TextView textView;
-
-    public MainActivity() {
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- sensorhelper/src/main/java/com/example/android/sensorhelper/SensorHelper.java
+++ sensorhelper/src/main/java/com/example/android/sensorhelper/SensorHelper.java
@@ -1,6 +1,5 @@
 package com.example.android.sensorhelper;
 
-import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
@@ -11,33 +10,35 @@ import android.hardware.SensorManager;
  * Created by rynel on 3/22/2018.
  */
 
-public class SensorHelper extends Activity implements SensorEventListener {
+public class SensorHelper implements SensorEventListener {
     private final SensorManager mSensorManager;
     private final Sensor mAccelerometer;
+    private Context context;
 
     double x, y, sensorX, sensorY;
 
+    public SensorHelper(SensorManager mSensorManager, Sensor mAccelerometer, Context context) {
+        this.mSensorManager = mSensorManager;
+        this.mAccelerometer = mAccelerometer;
+        this.context = context;
+    }
+
+    interface SensorCallback {
+        Object getSensor(SensorEvent event);
+
+        void setSensor(SensorEvent event);
+    }
+
     public SensorHelper() {
-        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         //if statement designating action as long as movement occurs
         if (mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).size() != 0) {
-            //do action here
-            Sensor s = mSensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+            //do action here;
         }
         //instantiating all double values to 0
         x = y = sensorX = sensorY = 0;
-    }
-
-    protected void onResume() {
-        super.onResume();
-        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    protected void onPause() {
-        super.onPause();
-        mSensorManager.unregisterListener(this);
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
