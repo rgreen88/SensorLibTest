52fbd2b436004ff81cac09d43342bed079e4c43e
rgreen88 <rynelgreen@gmail.com>
2018-03-24 11:37:57 -0400

    started Gang of Four. Singleton done, factory and more..

---
+++ designPatterns/src/main/java/com/example/android/designpatterns/Factory.java
@@ -0,0 +1,8 @@
+package com.example.android.designpatterns;
+
+/**
+ * Created by rynel on 3/24/2018.
+ */
+
+public class Factory {
+}
---
+++ designPatterns/src/main/java/com/example/android/designpatterns/Singleton.java
@@ -0,0 +1,21 @@
+package com.example.android.designpatterns;
+
+public class Singleton {
+
+
+    private static Singleton singleton = null;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) {
+            singleton = new Singleton();
+        }
+        return singleton;
+    }
+
+    public void sayHello() {
+        System.out.println("Hello");
+    }
+}
